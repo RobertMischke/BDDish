d1da6570f12160a0ca60607c6655a28159a47d1b
Robert Mischke <robertmischke@gmail.com>
2010-10-01 21:34:51 +0200

    initial project structure

---
+++ scr/Core/Model/Context.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BDDish
+{
+	public class Context
+	{
+	}
+}
---
+++ scr/Tests/Playground.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace BDDish.Tests
+{
+	public class Playground
+	{
+		[Test]
+		public void Playing_arround()
+		{
+					
+		}
+
+	}
+}
