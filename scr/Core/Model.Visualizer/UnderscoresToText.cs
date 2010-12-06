9d527ae6a9d4b389a1911ff8cb4d8bf9175608ca
Robert Mischke <robertmischke@gmail.com>
2010-12-06 19:42:12 +0100

    Underscore text to normal text

---
+++ scr/Core/Model.Visualizer/UnderscoresToText.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BDDish.Model.Visualizer
+{
+    public class UnderscoresToText
+    {
+        public string GetText(string underscoreText)
+        {
+            return underscoreText.Replace('_', ' ');
+        }
+    }
+}
