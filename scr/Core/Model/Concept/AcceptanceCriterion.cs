10b80292648db8139e89288892b9b0e1a693eb5c
Robert Mischke <robertmischke@gmail.com>
2010-12-16 13:30:13 +0100

    SpecificationNode to namespace BDDish.Model.Tree

--- scr/Core/Model/Concept/AcceptanceCriterion.cs
+++ scr/Core/Model/Concept/AcceptanceCriterion.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BDDish.Model.Tree;
 
 namespace BDDish.Model
 {
--- scr/Core/Model/Concept/Context/Assertion.cs
+++ scr/Core/Model/Concept/Context/Assertion.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BDDish.Model.Tree;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
 
