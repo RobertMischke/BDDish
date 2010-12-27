6ea7a2e65d4db1de1c2474d9fdc6e10abf89120c
Robert Mischke <robertmischke@gmail.com>
2010-12-27 13:25:34 +0100

    Centralized assembly info (removed AssemblyFileVersion, which seemed to prevent the correct working)

--- scr/AssemblyVersionAndPublisherInfo.cs
+++ scr/AssemblyVersionAndPublisherInfo.cs
@@ -25,6 +25,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Flavor=Retail")] // a.k.a. "Comments"
 #endif
 
-
 [assembly: AssemblyVersion("0.9.0.*")]
-[assembly: AssemblyFileVersion("0.9.0.*")]
--- scr/Core/Properties/AssemblyInfo.cs
+++ scr/Core/Properties/AssemblyInfo.cs
@@ -10,3 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("973ea4ef-f8db-46c2-bcef-6ad949dcbc17")]
 
+
