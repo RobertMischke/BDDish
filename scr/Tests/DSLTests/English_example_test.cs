d5f16983c1dcda2aec4a008e609f015454680560
Robert Mischke <robertmischke@gmail.com>
2011-03-30 18:32:16 +0200

    English DSL (wip)

---
+++ scr/Tests/DSLTests/English_example_test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace BDDish.Tests
+{
+    public class English_example_test
+    {
+        [Test]
+        public void Example_Test()
+        {
+            //new English.Feature("Example Feature")
+            //    .Requirement("Some customer needs something")
+            //    .As("the default customer").
+
+            /* 
+             * Feature(Example).
+             * Requirement("Sth should be")
+             * For("the given customer")
+             * 
+             * Given(this context)
+             * When()
+             * Then()
+             */
+
+            /* 
+             * Feature(Example).
+             * Requirement("Sth should be")
+             * As("the given customer")
+             * 
+             * Given(this context)
+             * When()
+             * ItsExpectedThat()
+             */
+        }
+    }
+}
